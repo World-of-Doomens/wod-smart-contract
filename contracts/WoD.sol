88ef075e3c2a77573c68f42787114afe336f2a1f
pattharathon srithundorn <lemon.pattharathon@gmail.com>
2022-02-22 00:18:44 +0700

    Update WoD unit test script

--- contracts/WoD.sol
+++ contracts/WoD.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.11;
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "erc721a/contracts/ERC721A.sol";
+import "./interface/IWOD.sol";
 
 contract WoD is ERC721A, Ownable {
     using MerkleProof for bytes32[];
@@ -11,6 +12,7 @@ contract WoD is ERC721A, Ownable {
     bytes32 public merkleRoot;
     bool public merkleEnabled = true;
     string private baseURI;
+    uint256 public constant MINT_LIMIT = 5;
     uint256 public constant MAX_SUPPLY = 8888;
     uint256 public constant PRICE = 0.05 * 1e18;
 
@@ -27,13 +29,23 @@ contract WoD is ERC721A, Ownable {
         baseURI = _baseUri;
     }
 
+    function numberMinted(address owner) public view returns (uint256) {
+        return _numberMinted(owner);
+    }
+
+    function exists(uint256 tokenId) public view returns (bool) {
+        return _exists(tokenId);
+    }
+
     function mint(bytes32[] memory proof, uint256 amount) external payable {
         if (merkleEnabled) {
             bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
             require(proof.verify(merkleRoot, leaf), "Invalid merkle proof! You might not in the whitelisted");
         }
 
+        uint256 numberOfMinted = numberMinted(msg.sender);
         require(amount > 0, "Can not mint zero token!");
+        require(numberOfMinted + amount <= MINT_LIMIT, "Can not mint token more than limit");
         require(msg.value == PRICE * amount, "Invalid amount of ether to buy token!");
 
         _safeMint(msg.sender, amount);
---
+++ test/WoD.test.js
@@ -0,0 +1,134 @@
+const { expect } = require('chai');
+const { constants } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
+
+describe('WoD', function () {
+  beforeEach(async function () {
+    const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+
+    var whitelistAddresses = [
+      owner.address.toString(), 
+      addr1.address.toString(), 
+      addr2.address.toString(), 
+      addr3.address.toString()
+    ]
+
+    this.leafNodes = whitelistAddresses.map(addr => keccak256(addr));
+    this.merkleTree = new MerkleTree(this.leafNodes, keccak256, { sortPairs: true });
+    this.rootHash = this.merkleTree.getHexRoot();
+
+    this.WOD = await ethers.getContractFactory('WoD');
+    this.wod = await this.WOD.deploy(this.rootHash, 'TOKEN URI');
+    await this.wod.deployed();
+  });
+
+  context('World of Doomens test cases', async function () {
+    beforeEach(async function () {
+      const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+      this.owner = owner;
+      this.addr1 = addr1;
+      this.addr2 = addr2;
+      this.addr3 = addr3;
+
+      const leaf = Buffer.from(keccak256(owner.address.toString()))
+      const index = this.merkleTree.getLeafIndex(leaf)
+      this.ownerProof = this.merkleTree.getHexProof(this.leafNodes[index]);
+
+      const leaf1 = Buffer.from(keccak256(addr1.address.toString()))
+      const index1 = this.merkleTree.getLeafIndex(leaf1)
+      this.addr1Proof = this.merkleTree.getHexProof(this.leafNodes[index1]);
+
+      const leaf2 = Buffer.from(keccak256(addr2.address.toString()))
+      const index2 = this.merkleTree.getLeafIndex(leaf2)
+      this.addr2Proof = this.merkleTree.getHexProof(this.leafNodes[index2]);
+
+      const leaf3 = Buffer.from(keccak256(addr3.address.toString()))
+      const index3= this.merkleTree.getLeafIndex(leaf3)
+      this.addr3Proof = this.merkleTree.getHexProof(this.leafNodes[index3]);
+    });
+
+    describe('Mint function with proof', async function () {
+      it('Can not mint token if proof is invalid', async function () {
+        await expect(this.wod.connect(this.addr1).mint(this.ownerProof,1,{value:ethers.utils.parseEther("0.05")}))
+        .to.be.revertedWith('Invalid merkle proof! You might not in the whitelisted');
+      });
+    
+      it('Can mint a token if proof is valid', async function () {
+        expect(await this.wod.mint(this.ownerProof, 1, { value: ethers.utils.parseEther("0.05") }));
+      });
+
+      it('Can mint multiple tokens if proof is valid', async function () {
+        expect(await this.wod.mint(this.ownerProof, 5, { value: ethers.utils.parseEther("0.25") }));
+      });
+
+      it('Can not mint token more than limit', async function () {
+        await expect(this.wod.mint(this.ownerProof, 6, {value: ethers.utils.parseEther("0.3")}))
+        .to.be.revertedWith('Can not mint token more than limit');
+      });
+
+      it('Can not mint token if ether is not enough', async function () {
+        await expect(this.wod.mint(this.ownerProof, 1, {value: ethers.utils.parseEther("0.01")}))
+        .to.be.revertedWith('Invalid amount of ether to buy token!');
+      });
+
+      it('Requires mint amount to be greater than 0', async function () {
+        await expect(this.wod.mint(this.ownerProof, 0, { value: ethers.utils.parseEther("0.05") }))
+        .to.be.revertedWith(
+          'Can not mint zero token!'
+        );
+      });
+    });
+
+    describe('Mint function without proof', async function () {
+      it('Can mint token without proof if merkle-tree is disabled', async function () {
+        await this.wod.disableMerkleProof();
+        expect (await this.wod.mint([], 1, { value: ethers.utils.parseEther("0.05") }));
+      });
+    })
+
+    describe('Check token exists', async function () {
+      beforeEach(async function () {
+        const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+        this.owner = owner;
+        this.addr1 = addr1;
+        this.addr2 = addr2;
+        this.addr3 = addr3;
+        await this.wod.connect(this.addr1).mint(this.addr1Proof, 1, { value: ethers.utils.parseEther("0.05") });
+        await this.wod.connect(this.addr2).mint(this.addr2Proof, 2, { value: ethers.utils.parseEther("0.1") });
+        await this.wod.connect(this.addr3).mint(this.addr3Proof, 3, { value: ethers.utils.parseEther("0.15") });
+      });
+
+      it('verifies valid tokens', async function () {
+        for (let tokenId = 0; tokenId < 6; tokenId++) {
+          const exists = await this.wod.exists(tokenId);
+          expect(exists).to.be.true;
+        }
+      });
+
+      it('verifies invalid tokens', async function () {
+        const exists = await this.wod.exists(6);
+        expect(exists).to.be.false
+      });
+
+      it('returns the amount for a given address', async function () {
+        expect(await this.wod.balanceOf(this.owner.address)).to.equal('0');
+        expect(await this.wod.balanceOf(this.addr1.address)).to.equal('1');
+        expect(await this.wod.balanceOf(this.addr2.address)).to.equal('2');
+        expect(await this.wod.balanceOf(this.addr3.address)).to.equal('3');
+      });
+
+      it('throws an exception for the 0 address', async function () {
+        await expect(this.wod.balanceOf(ZERO_ADDRESS)).to.be.revertedWith('BalanceQueryForZeroAddress');
+      });
+
+      it('returns the amount for a given address', async function () {
+        expect(await this.wod.numberMinted(this.owner.address)).to.equal('0');
+        expect(await this.wod.numberMinted(this.addr1.address)).to.equal('1');
+        expect(await this.wod.numberMinted(this.addr2.address)).to.equal('2');
+        expect(await this.wod.numberMinted(this.addr3.address)).to.equal('3');
+      });
+    });
+  });
+});
