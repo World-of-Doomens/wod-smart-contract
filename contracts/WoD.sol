eb853067ad8db874a80cd56de0b527e611a0b9a3
pattharathon srithundorn <lemon.pattharathon@gmail.com>
2022-02-22 21:44:42 +0700

    Add check whitelist function

--- contracts/WoD.sol
+++ contracts/WoD.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.11;
 
-import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "erc721a/contracts/ERC721A.sol";
+import {MerkleProof} from '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import 'erc721a/contracts/ERC721A.sol';
 
 contract WoD is ERC721A, Ownable {
     using MerkleProof for bytes32[];
@@ -15,7 +15,7 @@ contract WoD is ERC721A, Ownable {
     uint256 public constant MAX_SUPPLY = 8888;
     uint256 public constant PRICE = 0.05 * 1e18;
 
-    constructor(bytes32 _merkleRoot, string memory _baseUri) ERC721A("World of Doomens", "WODS") {
+    constructor(bytes32 _merkleRoot, string memory _baseUri) ERC721A('World of Doomens', 'WODS') {
         merkleRoot = _merkleRoot;
         baseURI = _baseUri;
     }
@@ -36,26 +36,31 @@ contract WoD is ERC721A, Ownable {
         return _exists(tokenId);
     }
 
+    function isWhitelisted(bytes32[] memory proof) external view returns (bool) {
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        return proof.verify(merkleRoot, leaf);
+    }
+
     function mint(bytes32[] memory proof, uint256 amount) external payable {
         if (merkleEnabled) {
             bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-            require(proof.verify(merkleRoot, leaf), "Invalid merkle proof! You might not in the whitelisted");
+            require(proof.verify(merkleRoot, leaf), 'Invalid merkle proof! You might not in the whitelisted');
         }
 
         uint256 numberOfMinted = numberMinted(msg.sender);
-        require(amount > 0, "Can not mint zero token!");
-        require(numberOfMinted + amount <= MINT_LIMIT, "Can not mint token more than limit");
-        require(msg.value == PRICE * amount, "Invalid amount of ether to buy token!");
+        require(amount > 0, 'Can not mint zero token!');
+        require(numberOfMinted + amount <= MINT_LIMIT, 'Can not mint token more than limit');
+        require(msg.value == PRICE * amount, 'Invalid amount of ether to buy token!');
 
         _safeMint(msg.sender, amount);
     }
 
     function withdraw() external payable onlyOwner {
-        uint balance = address(this).balance;
-        require(balance > 0, "No ether left to withdraw");
+        uint256 balance = address(this).balance;
+        require(balance > 0, 'No ether left to withdraw');
 
-        (bool success, ) = (msg.sender).call{value: balance}("");
-        require(success, "Withdraw failed.");
+        (bool success, ) = (msg.sender).call{value: balance}('');
+        require(success, 'Withdraw failed.');
     }
 
     function getEtherBalance() external view returns (uint256) {
